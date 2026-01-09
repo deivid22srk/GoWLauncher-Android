aab346a912ea1ac85c8e90bc73b7bbcf0e3e0954
deivid22srk <psvstore01@gmail.com>
2026-01-09 12:33:57 +0000

    Recriado automaticamente em 2026-01-09 12:33:57

---
+++ app/src/main/java/com/winlator/cmod/GlobalConfigActivity.java
@@ -0,0 +1,160 @@
+package com.winlator.cmod;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import com.winlator.cmod.container.Container;
+import com.winlator.cmod.container.ContainerManager;
+import com.winlator.cmod.core.DefaultVersion;
+
+import java.util.ArrayList;
+
+public class GlobalConfigActivity extends AppCompatActivity {
+
+    private Spinner spinnerDXVK;
+    private Spinner spinnerFEXCore;
+    private Spinner spinnerTurnip;
+    private Spinner spinnerResolution;
+    private Button btSaveConfig;
+    
+    private SharedPreferences sharedPreferences;
+    
+    private static final String PREF_GLOBAL_DXVK_VERSION = "global_dxvk_version";
+    private static final String PREF_GLOBAL_FEXCORE_VERSION = "global_fexcore_version";
+    private static final String PREF_GLOBAL_TURNIP_VERSION = "global_turnip_version";
+    private static final String PREF_GLOBAL_RESOLUTION = "global_resolution";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.global_config_activity);
+
+        MaterialToolbar toolbar = findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Configurações Globais");
+        }
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        spinnerDXVK = findViewById(R.id.SpinnerDXVK);
+        spinnerFEXCore = findViewById(R.id.SpinnerFEXCore);
+        spinnerTurnip = findViewById(R.id.SpinnerTurnip);
+        spinnerResolution = findViewById(R.id.SpinnerResolution);
+        btSaveConfig = findViewById(R.id.BTSaveConfig);
+
+        setupSpinners();
+        
+        btSaveConfig.setOnClickListener(v -> saveGlobalConfig());
+    }
+
+    private void setupSpinners() {
+        String[] resolutions = {"854x480", "1280x720", "1366x768", "1600x900", "1920x1080", "2560x1440"};
+        ArrayAdapter<String> resolutionAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, resolutions);
+        resolutionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerResolution.setAdapter(resolutionAdapter);
+        
+        String savedResolution = sharedPreferences.getString(PREF_GLOBAL_RESOLUTION, "1920x1080");
+        for (int i = 0; i < resolutions.length; i++) {
+            if (resolutions[i].equals(savedResolution)) {
+                spinnerResolution.setSelection(i);
+                break;
+            }
+        }
+
+        String[] dxvkVersions = {"2.6.2-1-arm64ec-gplasync", "2.3.1-arm64ec-gplasync", "2.3.1", "1.10.3-arm64ec-async", "1.10.3"};
+        ArrayAdapter<String> dxvkAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dxvkVersions);
+        dxvkAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDXVK.setAdapter(dxvkAdapter);
+        
+        String savedDXVK = sharedPreferences.getString(PREF_GLOBAL_DXVK_VERSION, "2.6.2-1-arm64ec-gplasync");
+        for (int i = 0; i < dxvkVersions.length; i++) {
+            if (dxvkVersions[i].equals(savedDXVK)) {
+                spinnerDXVK.setSelection(i);
+                break;
+            }
+        }
+
+        String[] fexVersions = {"2601", "2508"};
+        ArrayAdapter<String> fexAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, fexVersions);
+        fexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFEXCore.setAdapter(fexAdapter);
+        
+        String savedFex = sharedPreferences.getString(PREF_GLOBAL_FEXCORE_VERSION, "2601");
+        for (int i = 0; i < fexVersions.length; i++) {
+            if (fexVersions[i].equals(savedFex)) {
+                spinnerFEXCore.setSelection(i);
+                break;
+            }
+        }
+
+        String[] turnipVersions = {"turnip25.1.0", "v819"};
+        ArrayAdapter<String> turnipAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, turnipVersions);
+        turnipAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTurnip.setAdapter(turnipAdapter);
+        
+        String savedTurnip = sharedPreferences.getString(PREF_GLOBAL_TURNIP_VERSION, "turnip25.1.0");
+        for (int i = 0; i < turnipVersions.length; i++) {
+            if (turnipVersions[i].equals(savedTurnip)) {
+                spinnerTurnip.setSelection(i);
+                break;
+            }
+        }
+    }
+
+    private void saveGlobalConfig() {
+        String dxvkVersion = spinnerDXVK.getSelectedItem().toString();
+        String fexVersion = spinnerFEXCore.getSelectedItem().toString();
+        String turnipVersion = spinnerTurnip.getSelectedItem().toString();
+        String resolution = spinnerResolution.getSelectedItem().toString();
+
+        sharedPreferences.edit()
+            .putString(PREF_GLOBAL_DXVK_VERSION, dxvkVersion)
+            .putString(PREF_GLOBAL_FEXCORE_VERSION, fexVersion)
+            .putString(PREF_GLOBAL_TURNIP_VERSION, turnipVersion)
+            .putString(PREF_GLOBAL_RESOLUTION, resolution)
+            .apply();
+
+        ContainerManager containerManager = new ContainerManager(this);
+        ArrayList<Container> containers = containerManager.getContainers();
+        
+        for (Container container : containers) {
+            String graphicsDriverConfig = "vulkanVersion=1.3;version=" + turnipVersion + ";blacklistedExtensions=;maxDeviceMemory=0;presentMode=mailbox;syncFrame=0;disablePresentWait=0;resourceType=auto;bcnEmulation=auto;bcnEmulationType=software;bcnEmulationCache=0";
+            container.setGraphicsDriverConfig(graphicsDriverConfig);
+            container.setScreenSize(resolution);
+
+            String dxwrapperConfig = "version=" + dxvkVersion + ",framerate=0,async=0,asyncCache=0,vkd3dVersion=" + DefaultVersion.VKD3D + ",vkd3dLevel=12_1,ddrawrapper=" + Container.DEFAULT_DDRAWRAPPER + ",csmt=3,gpuName=NVIDIA GeForce GTX 480,videoMemorySize=2048,strict_shader_math=1,OffscreenRenderingMode=fbo,renderer=gl";
+            container.setDXWrapperConfig(dxwrapperConfig);
+
+            container.setFEXCoreVersion(fexVersion);
+            container.saveData();
+        }
+
+        Toast.makeText(this, "Configurações globais salvas!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/winlator/cmod/HomeFragment.java
@@ -0,0 +1,242 @@
+package com.winlator.cmod;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.winlator.cmod.container.Container;
+import com.winlator.cmod.container.ContainerManager;
+import com.winlator.cmod.core.PreloaderDialog;
+import com.winlator.cmod.game.Game;
+import com.winlator.cmod.game.GameManager;
+import com.winlator.cmod.xenvironment.ImageFs;
+import com.winlator.cmod.xenvironment.ImageFsInstaller;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private View emptyCard;
+    private GameManager gameManager;
+    private GamesAdapter adapter;
+    private ContainerManager containerManager;
+    private PreloaderDialog preloaderDialog;
+
+    public static final int REQUEST_ADD_GAME = 1001;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameManager = new GameManager(requireContext());
+        containerManager = new ContainerManager(requireContext());
+        preloaderDialog = new PreloaderDialog(requireActivity());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        recyclerView = view.findViewById(R.id.RecyclerViewGames);
+        emptyCard = view.findViewById(R.id.EmptyCard);
+        ExtendedFloatingActionButton fabAddGame = view.findViewById(R.id.FABAddGame);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 2));
+
+        fabAddGame.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivityNew) {
+                ((MainActivityNew) getActivity()).navigateToAddGame();
+            }
+        });
+
+        checkInstallation();
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadGames();
+    }
+
+    private void checkInstallation() {
+        if (!ImageFs.find(requireContext()).isValid()) {
+            preloaderDialog.show(R.string.installing_system_files);
+            if (getActivity() instanceof AppCompatActivity) {
+                ImageFsInstaller.installIfNeeded((AppCompatActivity) getActivity(), () -> {
+                    preloaderDialog.close();
+                    if (isAdded()) {
+                        loadGames();
+                    }
+                });
+            }
+        }
+    }
+
+    private void loadGames() {
+        ArrayList<Game> games = gameManager.loadGames();
+        
+        if (games.isEmpty()) {
+            emptyCard.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        } else {
+            emptyCard.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            adapter = new GamesAdapter(games);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    private class GamesAdapter extends RecyclerView.Adapter<GamesAdapter.ViewHolder> {
+        private final ArrayList<Game> games;
+
+        public GamesAdapter(ArrayList<Game> games) {
+            this.games = games;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.game_card_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Game game = games.get(position);
+            holder.tvGameName.setText(game.name);
+
+            Bitmap icon = game.getIcon();
+            if (icon != null) {
+                holder.ivGameIcon.setImageBitmap(icon);
+            } else {
+                holder.ivGameIcon.setImageResource(R.drawable.icon_wine);
+            }
+
+            holder.itemView.setOnClickListener(v -> launchGame(game));
+
+            holder.btnMore.setOnClickListener(v -> {
+                PopupMenu popup = new PopupMenu(requireContext(), holder.btnMore);
+                popup.getMenuInflater().inflate(R.menu.game_item_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(menuItem -> {
+                    int itemId = menuItem.getItemId();
+                    if (itemId == R.id.action_delete) {
+                        deleteGame(game);
+                        return true;
+                    }
+                    return false;
+                });
+                popup.show();
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return games.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView ivGameIcon;
+            TextView tvGameName;
+            ImageButton btnMore;
+
+            ViewHolder(View view) {
+                super(view);
+                ivGameIcon = view.findViewById(R.id.IVGameIcon);
+                tvGameName = view.findViewById(R.id.TVGameName);
+                btnMore = view.findViewById(R.id.BTMore);
+            }
+        }
+    }
+
+    private void launchGame(Game game) {
+        Container container = containerManager.getContainerById(game.containerId);
+        
+        if (container == null) {
+            Toast.makeText(requireContext(), "Container não encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        File exeFile = new File(game.executablePath);
+        if (!exeFile.exists()) {
+            Toast.makeText(requireContext(), "Arquivo executável não encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            String displayName = game.name;
+            String androidPath = game.executablePath;
+            String workDir = exeFile.getParent();
+
+            File shortcutsDir = container.getDesktopDir();
+            if (!shortcutsDir.exists()) shortcutsDir.mkdirs();
+
+            File desktopFile = new File(shortcutsDir, displayName + ".desktop");
+
+            try (PrintWriter writer = new PrintWriter(new FileWriter(desktopFile))) {
+                writer.println("[Desktop Entry]");
+                writer.println("Name=" + displayName);
+                writer.println("Exec=env WINEPREFIX=\"/home/xuser/.wine\" wine \"" + androidPath + "\"");
+                writer.println("Type=Application");
+                writer.println("Terminal=false");
+                writer.println("StartupNotify=true");
+                writer.println("Icon=" + displayName);
+                writer.println("Path=" + workDir);
+                writer.println("container_id:" + container.id);
+                writer.println("");
+                writer.println("[Extra Data]");
+                writer.println("container_id=" + container.id);
+            }
+
+            Intent intent = new Intent(requireContext(), XServerDisplayActivity.class);
+            intent.putExtra("container_id", container.id);
+            intent.putExtra("shortcut_path", desktopFile.getAbsolutePath());
+            intent.putExtra("shortcut_name", displayName);
+            startActivity(intent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(requireContext(), "Erro ao iniciar jogo: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void deleteGame(Game game) {
+        new androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                .setTitle("Excluir Jogo")
+                .setMessage("Deseja excluir " + game.name + "?")
+                .setPositiveButton("Sim", (dialog, which) -> {
+                    gameManager.deleteGame(game.id);
+                    
+                    File iconFile = new File(game.iconPath);
+                    if (iconFile.exists()) {
+                        iconFile.delete();
+                    }
+                    
+                    loadGames();
+                    Toast.makeText(requireContext(), "Jogo excluído", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton("Não", null)
+                .show();
+    }
+}
